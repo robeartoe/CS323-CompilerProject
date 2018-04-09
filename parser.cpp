e2b0586e3af37f84b90566bb6b38b48d5b958d16
smshinn <smshinn@csu.fullerton.edu>
2018-04-08 20:59:26 -0700

    Add files via upload

---
+++ parser.cpp
@@ -0,0 +1,469 @@
+/*
+CPSC 323-02
+Names:	Stephen Shinn
+Michael Perna
+Robert Ruiz
+
+Project: Assignment 1
+*/
+
+#include "parser.h"
+using namespace std;
+
+Parser::Parser(string fileIn, string fileOut){
+	
+	input.open(fileIn);
+	output.open(fileOut);
+
+	test = lexer();
+}
+
+/*
+========================
+IMPLEMENTATION OF LEXER
+========================
+*/
+
+Token Parser::lexer(){			//lexer fuction
+
+	string lexeme;	//Strings that will contain data after processing
+	string token;
+	bool tokenFound = false;
+
+	char testChar;
+	testChar = input.get();
+	if(testChar = '\n')
+		lineNum++;
+
+	while (!tokenFound)
+	{
+		if (testChar == EOF)
+		{
+			lexeme.push_back(testChar);
+			token = "EOF";
+			return Token{token, lexeme};
+		}
+		else
+		if (testChar == '!')
+		{
+			do {
+				testChar = input.get();		//proceed to next character inside comment
+				if(testChar = '\n')
+					lineNum++;
+			} while(testChar != '!');		//iterate through all of comment
+
+			testChar = input.get();			//sets next character to be read
+			if(testChar = '\n')
+					lineNum++;
+		}
+		else
+		if(isspace(testChar))				//proceed through all whitespace
+		{
+			do {
+				testChar = input.get();
+				if(testChar = '\n')
+					lineNum++;
+			} while(isspace(testChar));
+		}
+		else
+		if (isalpha(testChar))	//FSM for id
+		{
+			lexeme.push_back(testChar);
+			int state = 2;
+			int prevState;
+			bool validState = true;
+
+			while (validState)
+			{
+				switch (state)
+				{
+					case 2:
+						testChar = input.get();
+						lexeme.push_back(testChar);
+						if(isalpha(testChar))
+							state = 4;
+						else
+						if(isdigit(testChar))
+							state = 5;
+						else
+						if(testChar == '$')
+							state = 3;
+						else
+						{
+							prevState = state;
+							state = 6;
+						}
+						break;
+					case 3:
+						testChar = input.get();
+						lexeme.push_back(testChar);
+						if(isalpha(testChar))
+						{
+							prevState = state;
+							state = 6;
+						}
+						else
+						if(isdigit(testChar))
+						{
+							prevState = state;
+							state = 6;
+						}
+						else
+						if(testChar == '$')
+						{
+							prevState = state;
+							state = 6;
+						}
+						else
+						{
+							prevState = state;
+							state = 6;
+						}
+						break;
+					case 4:
+						testChar = input.get();
+						lexeme.push_back(testChar);
+						if(isalpha(testChar))
+							state = 4;
+						else
+						if(isdigit(testChar))
+							state = 5;
+						else
+						if(testChar == '$')
+							state = 3;
+						else
+						{
+							prevState = state;
+							state = 6;
+						}
+						break;
+					case 5:
+						testChar = input.get();
+						lexeme.push_back(testChar);
+						if(isalpha(testChar))
+							state = 4;
+						else
+						if(isdigit(testChar))
+							state = 5;
+						else
+						if(testChar == '$')
+							state = 3;
+						else
+						{
+							prevState = state;
+							state = 6;
+						}
+						break;
+					case 6:
+						input.unget();
+						lexeme.pop_back();
+						validState = false;
+						break;
+				}
+			}
+
+			switch (prevState)
+			{
+				case 5:
+				case 6:
+				default:
+					token = "unrecognized";
+					break;
+				case 2:
+				case 3:
+					token = "identifier";
+					break;
+				case 4:
+					token = "identifier";
+					if (isKeyword(lexeme))
+						token = "keyword";
+					break;
+			}
+
+			tokenFound = true;
+		}
+		else
+		if (isdigit(testChar))	//FSM for int/real
+		{
+			lexeme.push_back(testChar);
+			int state = 2;
+			int prevState;
+			bool validState = true;
+
+			while (validState)
+			{
+				switch (state)
+				{
+					case 2:
+						testChar = input.get();
+						lexeme.push_back(testChar);
+						if(isdigit(testChar))
+							state = 2;
+						else
+						if(testChar == '.')
+							state = 3;
+						else
+						{
+							prevState = state;
+							state = 5;
+						}
+						break;
+					case 3:
+						testChar = input.get();
+						lexeme.push_back(testChar);
+						if(isdigit(testChar))
+							state = 4;
+						else
+						if(testChar == '.')
+						{
+							prevState = state;
+							state = 5;
+						}
+						else
+						{
+							prevState = state;
+							state = 5;
+						}
+						break;
+					case 4:
+						testChar = input.get();
+						lexeme.push_back(testChar);
+						if(isdigit(testChar))
+							state = 4;
+						else
+						if(testChar == '.')
+						{
+							prevState = state;
+							state = 5;
+						}
+						else
+						{
+							prevState = state;
+							state = 5;
+						}
+						break;
+					case 5:
+						input.unget();
+						lexeme.pop_back();
+						validState = false;
+						break;
+				}
+			}
+
+			switch (prevState)
+			{
+				case 2:
+					token = "integer";
+					break;
+				case 4:
+					token = "real";
+					break;
+				case 3:
+				case 5:
+					token = "unrecognized";
+					break;
+			}
+
+			tokenFound = true;
+		}
+		else
+		if (isOpSepChar(testChar))	//Check for op/sep
+		{
+			lexeme.push_back(testChar);
+			bool opSepFound = false;
+
+			while (!opSepFound)
+			{
+				char peek;
+
+				switch (testChar)
+				{
+					case '+':
+					case '-':
+					case '*':
+					case '/':
+					case '<':
+					case '>':
+					case ';':
+					case ':':
+					case ',':
+					case '{':
+					case '}':
+					case '(':
+					case ')':
+					case '[':
+					case ']':
+						opSepFound = true;
+						break;
+					//Special Case Characters
+					case '=':
+						peek = input.peek();
+						if ((peek == '>') || (peek == '<') || (peek == '='))
+						{
+							testChar = input.get();
+							lexeme.push_back(testChar);
+						}
+						opSepFound = true;
+						break;
+					case '^':
+						peek = input.peek();
+						if (peek == '=')
+						{
+							testChar = input.get();
+							lexeme.push_back(testChar);
+						}
+						opSepFound = true;
+						break;
+					case '%':
+						peek = input.peek();
+						if (peek == '%')
+						{
+							testChar = input.get();
+							lexeme.push_back(testChar);
+						}
+						opSepFound = true;
+						break;
+				}
+			}
+
+			if(isOperator(lexeme))
+				token = "operator";
+			else
+			if(isSeparator(lexeme))
+				token = "separator";
+			else
+				token = "unrecognized";
+
+			tokenFound = true;
+		}
+		else
+		{	
+			lexeme.push_back(testChar);
+			token = "unrecognized";
+			tokenFound = true;
+		}
+	}
+	return Token{token, lexeme};
+}
+
+bool Parser::isOpSepChar(char a)
+{
+	switch (a)
+	{
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '<':
+		case '>':
+		case ';':
+		case ':':
+		case ',':
+		case '{':
+		case '}':
+		case '(':
+		case ')':
+		case '[':
+		case ']':
+		//Special Case Characters
+		case '=':
+		case '^':
+		case '%': 
+			return true;
+		default:
+			break;
+	}
+
+	return false;
+}
+
+bool Parser::isKeyword(string lex)
+{
+	for (auto x : keywords)
+		if (lex.compare(x) == 0)
+			return true;
+	return false;
+}
+
+bool Parser::isOperator(string lex)
+{
+	for (auto x : operators)
+		if(lex.compare(x) == 0)
+			return true;
+	return false;
+}
+
+bool Parser::isSeparator(string lex)
+{
+	for(auto x : separators)
+		if(lex.compare(x) == 0)
+			return true;
+	return false;
+}
+
+bool Parser::iseof()
+{
+	if (input.peek() == EOF)
+		return true;
+	else
+		return false;
+}
+
+int Parser::currentLine()
+{
+	return lineNum;
+}
+
+/*
+==================================
+IMPLEMENTATION OF SYNTAX ANALYZER
+==================================
+*/
+
+void Parser::R18S() {
+
+	if (printRules)
+	{
+		cout << "<Rat18S> ::= <Opt Function Definitions> %% <Opt Declaration List> <Statement List>" << endl;
+		output << "<Rat18S> ::= <Opt Function Definitions> %% <Opt Declaration List> <Statement List>" << endl;
+	}
+
+	OFD();
+	
+	if (lexer().lexeme.compare("%%") == 0)
+	{
+		//ODL();
+		//SL();
+	}
+	else
+	{
+		cout << "Error: Expected %% in line " << lineNum;
+		output << "Error: Expected %% in line " << lineNum;
+	}
+		
+
+};
+
+void Parser::OFD() {
+
+	if (printRules)
+	{
+		cout << "<Opt Function Definitions> ::= <Function Definitions> | <Empty>" << endl;
+		output << "<Opt Function Definitions> ::= <Function Definitions> | <Empty>" << endl;
+	}
+
+	FD();
+
+}
+
+void Parser::FD() {
+	
+	if (printRules)
+	{
+		cout << "<Function Definitions> ::= <Function> | <Function> <Function Definitions>" << endl;
+		output << "<Function Definitions> ::= <Function> | <Function> <Function Definitions>" << endl;
+	}
+
+
+
+}
---
+++ parser.h
@@ -0,0 +1,57 @@
+/*
+CPSC 323-02
+Names:	Stephen Shinn
+Michael Perna
+Robert Ruiz
+
+Project: Assignment 1
+*/
+
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+struct Token {
+	std::string token;
+	std::string lexeme;
+};
+
+class Parser {
+	private:
+		//PRINT PRODUCTION RULES
+		bool printRules = true;
+
+		std::ifstream input;
+		std::ofstream output;
+
+		std::vector<std::string> keywords{ "int", "boolean", "real", "if", "endif", "else", "return", "put", "get", "while", "true", "false", "function" };
+		std::vector<std::string> operators{"+", "-", "*", "/", "<", ">", "=", "=>", "=<", "==", "^="};
+		std::vector<std::string> separators{";", ":", ",", "{", "}", "(", ")", "[", "]", "%%"};
+
+		int lineNum = 1;
+
+		Token test;
+
+		bool isOpSepChar(char a);
+		bool isKeyword(std::string lex);
+		bool isOperator(std::string lex);
+		bool isSeparator(std::string lex);
+
+		void Parser::match(std::string lexeme);
+		void Parser::OFD();
+		void Parser::FD();
+
+	public:
+		Parser::Parser(std::string fileIn, std::string fileOut);
+		Token lexer();
+		bool iseof();
+		int currentLine();
+		void Parser::R18S();
+
+};
+
+#endif
