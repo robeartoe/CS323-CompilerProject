e6b9d252f6864bdb2cee62874357e10bf6ba13dc
Robert Ruiz <rsblooxpert@gmail.com>
2018-05-05 13:29:15 -0700

    Check if it increments correctly in testFile

--- testfile.cpp
+++ testfile.cpp
@@ -4,9 +4,16 @@
 #include  <iostream>
 #include <cstdlib>
 
+//To compile: make -f makefile.dbg
+
 int main(int argc, char const *argv[]) {
   /* code */
   std::cout << "This is a test: " << '\n';
+  std::cout << "Memory_address is: "<< Memory_address << '\n';
+  std::cout << "instr_address is: " << instr_address << '\n';
+  std::cout << "OK, lets increment them: " << '\n';
+  Memory_address++;
+  instr_address++;
   std::cout << "Memory_address is: "<< Memory_address << '\n';
   std::cout << "instr_address is: " << instr_address << '\n';
   return 0;
